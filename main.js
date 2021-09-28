88848b0344467c7dfb313533d49e5222c4f617a6
angelinajosephine <91074692+angelinajosephine@users.noreply.github.com>
2021-09-28 21:16:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+var Name_of_The_Guest_Array = [];
+
+function Submit()
+    {
+        var Guest_name = document.getElementById("Enter_name").value;
+        Name_of_The_Guest_Array.push(Guest_name);
+        document.getElementById("Display_names").innerHtml = Name_of_The_Guest_Array;
+        console.log(Name_of_The_Guest_Array);
+        var length_of_the_array =Name_of_The_Guest_Array.length;
+        console.log(length_of_the_array);
+    }
+
+    function Show_list()
+        {
+            var i = Name_of_The_Guest_Array.join("<br>");
+            console.log(Name_of_The_Guest_Array);
+            document.getElementById("Show_names").innerHtml = i.toString();
+        }
+
+        function sorting()
+        {
+            Name_of_The_Guest_Array.sort();
+            var i = Name_of_The_Guest_Array.join("<br>");
+            console.log(Name_of_The_Guest_Array);
+            document.getElementById("Sorted_names").innerHtml = i.toString();
+        }
+
+        function Search()
+        {
+            var s = document.getElementById("Search_name").nodeValue;
+            var found =0;
+            var j;
+            for(j=0; j<Name_of_The_Guest_Array.length; j++){
+                if(s==Name_of_The_Guest_Array[j]){
+                    found = found+1;
+                }
+            }
+
+            document.getElementById("Search_names").innerHtl = "Name Found "+found+" Time/s";
+            console.log("Found Name "+found+" Time/s";)
+        }
+
+    
